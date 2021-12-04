781280cbd4258e91a113acef7590c092ddeec621
Shawn <81163565+Shawn-the-star@users.noreply.github.com>
2021-12-04 18:33:00 +0530

    Fixed an the ai and changed player config

--- ChessMain.py
+++ ChessMain.py
@@ -55,7 +55,7 @@ def main():
     playerClicks = [] # keep track of player clicks (two tuples: [(6,4), (4,4)])
     gameOver = False
     playerOne = True # if a human is playing white, then this will be True. If an AI is playing, then false
-    playerTwo = True # same as above but for black
+    playerTwo = False # same as above but for black
     while running:
         humanTurn = (gs.whiteToMove and playerOne) or (not gs.whiteToMove and playerTwo)
         for e in p.event.get():
